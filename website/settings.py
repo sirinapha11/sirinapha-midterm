b3cb2cf53dc2f9d2e6487e09945609ecd0cca8f1
film sirinapha <86333939+sirinapha11@users.noreply.github.com>
2021-09-11 02:21:18 +0700

    v

--- website/settings.py
+++ website/settings.py
@@ -79,6 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webapp',
+        'USER': 'djtraning',
+        'PASSWORD': '123459876',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
